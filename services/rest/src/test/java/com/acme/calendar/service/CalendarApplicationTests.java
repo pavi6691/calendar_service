0d8ef4725b6a995f7472be7080458cb1c417b7d8
KIMHRAC <kimhrac@gmail.com>
2024-01-12 10:46:31 +0100

    Added initial Spring Boot code

---
+++ services/rest/src/test/java/com/acme/calendar/service/CalendarApplicationTests.java
@@ -0,0 +1,15 @@
+package com.acme.calendar.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class CalendarApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
