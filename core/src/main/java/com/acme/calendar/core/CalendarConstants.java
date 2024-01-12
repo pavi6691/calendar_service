840957f053213009ff2edc91874210c597b0a346
KIMHRAC <kimhrac@gmail.com>
2024-01-12 14:25:30 +0100

    Added constants

--- core/src/main/java/com/acme/calendar/core/CalendarConstants.java
+++ core/src/main/java/com/acme/calendar/core/CalendarConstants.java
@@ -8,8 +8,16 @@ public class CalendarConstants {
      * REST endpoints - query parameters
      */
 
-    public static final String API_ENDPOINT_QUERY_PARAMETER_DELETE_PERMANENTLY = "deletePermanently";
+    public static final String API_QUERY_DELETE_PERMANENTLY = "deletePermanently";
 
+
+    /*
+     * REST endpoints - path parameters
+     */
+
+    public static final String API_PATH_SET_UUID = "uuid";
+    public static final String API_PATH_CALENDAR_UUID = "uuid";
+    public static final String API_PATH_EVENT_UUID = "uuid";
 
     /*
      * REST endpoints - paths
@@ -18,6 +26,7 @@ public class CalendarConstants {
     public static final String API_ENDPOINT_PREFIX = "/api/v1";
 
     public static final String API_ENDPOINT_DEVOPS = "/devops";
+
     public static final String API_ENDPOINT_SETS = "/sets";
     public static final String API_ENDPOINT_CALENDARS = "/calendars";
     public static final String API_ENDPOINT_EVENTS = "/events";
@@ -32,11 +41,18 @@ public class CalendarConstants {
     public static final String API_ENDPOINT_EVENTS_CREATE = "";
     // GET
     public static final String API_ENDPOINT_SETS_GET_ALL = "";
+    public static final String API_ENDPOINT_SETS_GET_BY_UUID = "/{" + API_PATH_SET_UUID + "}";
     public static final String API_ENDPOINT_CALENDARS_GET_ALL = "";
+    public static final String API_ENDPOINT_CALENDARS_GET_BY_UUID = "/{" + API_PATH_CALENDAR_UUID + "}";
     public static final String API_ENDPOINT_EVENTS_GET_ALL = "";
+    public static final String API_ENDPOINT_EVENTS_GET_BY_UUID = "/{" + API_PATH_EVENT_UUID + "}";
     // PUT
-    // ...
+    public static final String API_ENDPOINT_SETS_UPDATE = "";
+    public static final String API_ENDPOINT_CALENDARS_UPDATE = "";
+    public static final String API_ENDPOINT_EVENTS_UPDATE = "";
     // DELETE
-    // ...
+    public static final String API_ENDPOINT_SETS_DELETE = "";
+    public static final String API_ENDPOINT_CALENDARS_DELETE = "";
+    public static final String API_ENDPOINT_EVENTS_DELETE = "";
 
 }
