bcd74a1ce12edc9f0efe5ad733a257fbed1c6420
KIMHRAC <kimhrac@gmail.com>
2024-02-08 16:09:46 +0100

    Added missing test for CTimePeriod

---
+++ models/src/test/java/com/acme/calendar/model/common/CTimePeriodTest.java
@@ -0,0 +1,16 @@
+package com.acme.calendar.model.common;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class CTimePeriodTest {
+
+
+    @Test
+    @DisplayName("Test ...")
+    void testXXX() {
+
+    }
+
+}
