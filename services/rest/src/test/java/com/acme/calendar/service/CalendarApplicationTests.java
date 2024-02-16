c0cd1a180eba44be4b5e115181a6f40357b77667
KIMHRAC <kimhrac@gmail.com>
2024-02-16 10:24:32 +0100

    Disabled CalendarApplicationTests

--- services/rest/src/test/java/com/acme/calendar/service/CalendarApplicationTests.java
+++ services/rest/src/test/java/com/acme/calendar/service/CalendarApplicationTests.java
@@ -1,9 +1,11 @@
 package com.acme.calendar.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
+@Disabled
 @SpringBootTest
 public class CalendarApplicationTests {
 
