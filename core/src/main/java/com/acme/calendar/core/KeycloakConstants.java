59217931df1b954e28fb46643831274ea290e7ed
parveenkaradi <praveen.karadi@logicmonitor.com>
2024-04-22 16:53:39 +0530

    added more swagger docs

--- core/src/main/java/com/acme/calendar/core/KeycloakConstants.java
+++ core/src/main/java/com/acme/calendar/core/KeycloakConstants.java
@@ -17,9 +17,9 @@ public class KeycloakConstants {
     public static final String CREATE_USER = "/createUser";
     public static final String CREATE_GROUP = "/createGroup";
     public static final String CREATE_ROLE = "/createRole";
-    public static final String ASSIGN_ROLE = "/assignRole";
+    public static final String ASSIGN_ROLE = "/assignRoleUser";
     public static final String INVITE_USER = "/inviteUser";
-    public static final String ASSIGN_GROUP = "/assignGroup";
+    public static final String ASSIGN_GROUP = "/assignRoleGroup";
     public static final String ADD_USER_TO_GROUP = "/addUserToGroup";
     public static final String REVOKE_INVITE = "/roles/remove";
     public static final String LOGIN =  "/login";
